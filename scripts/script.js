0ceef0bbc4f299be46a432ea7601cdd55ff3042a
leau-void <lpierra@gmail.com>
2021-07-13 10:17:06 -0300

    made the playround, currently working on the outcome functions

--- scripts/script.js
+++ scripts/script.js
@@ -10,6 +10,62 @@ function computerPlay() {
 }
 
 function playerPlay() {
-    pick = prompt("Pick Rock, Paper or Scissors.").toLowerCase();
-    return pick;
+    pick = prompt("Pick Rock, Paper or Scissors.");
+
+    return (pick ?? invalidSelection());
+}
+
+function invalidSelection() {
+    alert('Your input was not valid. Try again.');
+    playRound(playerPlay(), computerPlay());
+}
+
+function playerWin() {
+console.log("player win");
+}
+
+function computerWin() {
+console.log("computer win");
+}
+
+function tie() {
+console.log("tie")
+}
+
+function game() {
+    playRound(playerPlay(), computerPlay());
+}
+
+function playRound(playerSelection, computerSelection) {
+    let result;
+console.log(`${playerSelection}, ${computerSelection}`);
+if (playerSelection === computerSelection) {
+    result = tie;
+} else {
+    if (playerSelection == "rock") {
+        if (computerSelection === "scissors") {
+            result = playerWin;
+        } else if (computerSelection === "paper") {
+            result = computerWin;
+        }
+
+    } else if (playerSelection === "paper") {
+        if (computerSelection === "rock") {
+            result = playerWin;
+        } else if (computerSelection === "scissors") {
+            result = computerWin;
+        }
+
+    } else if (playerSelection === "scissors") {
+        if (computerSelection === "paper") {
+            result = playerWin;
+        } else if (computerSelection === "rock") {
+            result = computerWin;
+        }
+
+    } else {
+        result = invalidSelection;
+    }
+}
+return result();
 }
